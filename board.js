94ca4e1bba6bc5b5f11b3bc10b7bd24a2a5047c3
Jessie <lonsdale.jess@gmail.com>
2019-12-19 20:41:47 +0100

    tidied up refactored

--- board.js
+++ board.js
@@ -5,6 +5,7 @@ class Board {
     const numbers = [1,2,3,4,5,6,7,8]
     this.board = []
     this.filledSquares = []
+    this.filled = false
     letters.forEach((letter) => {
       numbers.forEach((number)=> {
         this.board.push(new Square(letter+number))
@@ -23,49 +24,30 @@ class Board {
 
   fillBoard(square) {
     this.filledSquares.push(square)
-
-    this.filledSquares.forEach( (item)=> {
-      this.nextMoves(item)
+    const arr = [1,2,3,4,5,6]
+    arr.forEach((i) => {
+      this.filledSquares.forEach( (item)=> {
+        this.nextMoves(item)
+      })
     })
-    this.filledSquares.forEach( (item)=> {
-      this.nextMoves(item)
-    })
-    this.filledSquares.forEach( (item)=> {
-      this.nextMoves(item)
-    })
-    this.filledSquares.forEach( (item)=> {
-      this.nextMoves(item)
-    })
-    this.filledSquares.forEach( (item)=> {
-      this.nextMoves(item)
-    })
-    this.filledSquares.forEach( (item)=> {
-      this.nextMoves(item)
-    })
-
-
-    this.filledSquares = []
-
+    this.filled = true
   }
 
   nextMoves(square) {
-      var currentSquares = []
-      const currentX = square.x;
-      const currentY = square.y;
-      const arr = [1,2, 2,1, -1,-2, -2,-1, -1,2, 2,-1, 1,-2, -2,1]
-      for(var i=0; i<arr.length; i+=2) {
-        var x=currentX+arr[i]
-        var y=currentY+arr[i+1]
-        var currentSquare = this.findByCoords(x,y)
-        if(x<=8&&x>=1&&y<=8&&y>=1&&currentSquare.filled==false) {
-          this.filledSquares.push(currentSquare)
-          currentSquare.fillSquare(square.value+1)
-          $('#'+currentSquare.id).text(currentSquare.value);
-          $('#'+currentSquare.id).css("background", currentSquare.colour());
-        }
+    const currentX = square.x;
+    const currentY = square.y;
+    const arr = [1,2, 2,1, -1,-2, -2,-1, -1,2, 2,-1, 1,-2, -2,1]
+    for(var i=0; i<arr.length; i+=2) {
+      var x=currentX+arr[i]
+      var y=currentY+arr[i+1]
+      var currentSquare = this.findByCoords(x,y)
+      if(x<=8&&x>=1&&y<=8&&y>=1&&currentSquare.filled==false) {
+        this.filledSquares.push(currentSquare)
+        currentSquare.fillSquare(square.value+1)
+        $('#'+currentSquare.id).text(currentSquare.value);
+        $('#'+currentSquare.id).css("background", currentSquare.colour());
       }
     }
-
-
+  }
 
 }
--- interface.js
+++ interface.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
   chessBoard = new Board()
 
   $('.grid-item').click(function() {
-    console.log(this.id)
-    $('#'+this.id).text("0");
-    square = chessBoard.findByID(this.id)
-    square.fillSquare(0)
-    chessBoard.fillBoard(square)
-  });
-});
+    if(chessBoard.filled == false) {
+      $('#'+this.id).text("0");
+      square = chessBoard.findByID(this.id)
+      square.fillSquare(0)
+      chessBoard.fillBoard(square)
+    }
+  })
+})
